ead1d56fd47ceb00ac999707980c09017d311d0b
masumomo <masumotomiki18@gmail.com>
2024-05-18 12:37:29 +0900

    Have 1 more step

--- customhandler/main.go
+++ customhandler/main.go
@@ -11,13 +11,12 @@ func main() {
 	handler := myhandler.New(os.Stdout, nil)
 	logger := slog.New(handler).With("serviceName", "payment service").
 		WithGroup("ProcessTxnFunc")
-	slog.SetDefault(logger)
 
-	slog.Info("New payment",
+	logger.Info("New payment",
 		slog.Group("Transaction",
 			slog.Int64("JPYAmount", 980),
 			slog.String("Marchant", "Abema.tv"),
 		),
 	)
-	slog.Warn("Unknown currency", slog.String("CurrenyCode", "???"))
+	logger.Warn("Unknown currency", slog.String("CurrenyCode", "???"))
 }
