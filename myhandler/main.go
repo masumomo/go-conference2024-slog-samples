63a2e86bf7c5d6f2571b3fa513d1e7a07b753bdf
masumomo <masumotomiki18@gmail.com>
2024-05-19 17:53:54 +0900

    Refacter main.go

--- myhandler/main.go
+++ myhandler/main.go
@@ -4,20 +4,58 @@ import (
 	"log/slog"
 	"os"
 
-	myhandler "github.com/masumomo/goconference2024_slog_demos/myhandler/step1"
+	"github.com/masumomo/goconference2024_slog_demos/myhandler/final"
+	"github.com/masumomo/goconference2024_slog_demos/myhandler/step1"
 )
 
 func main() {
-	handler := myhandler.New(os.Stdout, nil)
+	handler := final.New(os.Stdout, nil)
 	logger := slog.New(handler).
 		With("serviceName", "payment service"). // Output service name
 		WithGroup("ProcessTxnFunc")             // Output within function name
 
-	logger.Info("New payment",
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "step1":
+			handler := step1.New(os.Stdout, nil)
+			logger = slog.New(handler).
+				With("serviceName", "payment service"). // Output service name
+				WithGroup("ProcessTxnFunc")             // Output within function name
+
+		case "step2":
+			handler := step1.New(os.Stdout, nil)
+			logger = slog.New(handler).
+				With("serviceName", "payment service"). // Output service name
+				WithGroup("ProcessTxnFunc")             // Output within function name
+
+		case "step3":
+			handler := step1.New(os.Stdout, nil)
+			logger = slog.New(handler).
+				With("serviceName", "payment service"). // Output service name
+				WithGroup("ProcessTxnFunc")             // Output within function name
+
+		case "step4":
+			handler := step1.New(os.Stdout, nil)
+			logger = slog.New(handler).
+				With("serviceName", "payment service"). // Output service name
+				WithGroup("ProcessTxnFunc")             // Output within function name
+
+		case "step5":
+			handler := step1.New(os.Stdout, nil)
+			logger = slog.New(handler).
+				With("serviceName", "payment service"). // Output service name
+				WithGroup("ProcessTxnFunc")             // Output within function name
+
+		}
+	}
+	slog.SetDefault(logger)
+
+	slog.Info("New payment",
 		slog.Group("Transaction",
 			slog.Int64("JPYAmount", 980),
 			slog.String("Marchant", "Abema.tv"),
 		),
 	)
-	logger.Warn("Unknown currency", slog.String("CurrenyCode", "???"))
+	// Some logic
+	slog.Warn("Unknown currency", slog.String("CurrenyCode", "???"))
 }
